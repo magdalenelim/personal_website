ee6814f0af80dbe16bb24bf025d17c305718b007
qylim <qylim@berkeley.edu>
2014-08-23 17:37:27 -0700

    woohoo home page up

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def projects
+  end
+
+  def resume
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 PersonalWebsite::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/projects"
+  get "static_pages/resume"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
