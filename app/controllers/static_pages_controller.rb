07cd3da5ece09a981db3bb6cd9c2302d8a0d8a64
qylim <qylim@berkeley.edu>
2014-09-06 20:28:02 -0700

    resume page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,8 +9,16 @@ class StaticPagesController < ApplicationController
   end
 
   def resume
+    # send_file('/Users/magdalenelim/Desktop/rails_apps/personal_website/app/assets/', :filename => 'Resume-1copy.pdf', :disposition => 'inline', :type => "application/pdf") 
+    pdf
   end
 
   def contact
   end
+
+  def pdf
+    pdf_filename = File.join(Rails.root, "app/assets/Resume-1copy.pdf")
+    send_file(pdf_filename, :filename => "Resume-1copy.pdf",:disposition => 'inline', :type => "application/pdf")
+  end
+
 end
